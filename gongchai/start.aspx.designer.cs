7c4580f91a592da9509cbee57ef7a4d027800e65
DAKINGLIU <53357407+DAKINGLIU@users.noreply.github.com>
2019-07-29 11:22:38 +0800

    Update and rename gongchai/start.aspx.designer.cs to  gongchai/start.aspx.designer.cs

--- gongchai/start.aspx.designer.cs
+++ gongchai/start.aspx.designer.cs
@@ -1,13 +1,13 @@
-﻿//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // <自动生成>
 //     此代码由工具生成。
 //
-//     对此文件的更改可能导致不正确的行为，如果
-//     重新生成代码，则所做更改将丢失。
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
 // </自动生成>
 //------------------------------------------------------------------------------
 
-namespace WebApplication1 {
+namespace start {
     
     
     public partial class start {
@@ -22,48 +22,57 @@ namespace WebApplication1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// 公差 控件。
+        /// Label1 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label 公差;
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// 公差自动分配 控件。
+        /// Label2 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button 公差自动分配;
+        protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
-        /// 公差手动分配 控件。
+        /// Button3 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button 公差手动分配;
+        protected global::System.Web.UI.WebControls.Button Button3;
         
         /// <summary>
-        /// 公差计分 控件。
+        /// Button4 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button 公差计分;
+        protected global::System.Web.UI.WebControls.Button Button4;
         
         /// <summary>
-        /// 人员信息设置 控件。
+        /// Button1 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button 人员信息设置;
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Button2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
